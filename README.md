2d791b35b1460c3b2815a4c3a6bde758f8bf033a
myNBGcode <developer@nbg.gr>
2019-02-04 09:07:16 +0200

    Python Template complete change
    Python Template complete change

--- README.md
+++ README.md
@@ -12,4 +12,14 @@ Read the following instructions in order start working frictionlessly in NBG Dev
 
 5. If you have any thoughts, issues or proposals for SourceLair, it would be great if you could send us an email at support@sourcelair.com
 
+## About the Python Template
+
+Nbg's Python template is a minimal host template that serves a simple .html index page
+
+That page is consisted by three links which you can click and then a python script is called which makes a call to NBG's API
+
+In order to create your personal sandbox in your project you need to change the REPLACE_THIS_VALUE parameter of the app.py file to match your personal data
+
+The output of the scripts isn't visible in the public url of your project, but it is, in the server logs
+
 Have fun coding in your browser!
---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return render_template('index.html')
+    
+
+
+@app.route('/createsandbox')
+def createsandbox():
+    url = "https://apis.nbg.gr/public/sandbox/obp.account.sandbox/v1.1/sandbox"
+    data = '''{"sandbox_id":"REPLACE_THIS_VALUE"}'''
+    headers = {"x-ibm-client-id": "REPLACE_THIS_VALUE",
+                "content-type": "application/json",
+                "accept": "application/json"}
+    myResponse = requests.post(url, data = data, headers = headers)
+    data= myResponse.content
+    print (data)
+    return (data)
+
+@app.route('/deletesandbox')
+def deletesandbox():
+    url = "https://apis.nbg.gr/public/sandbox/obp.account.sandbox/v1.1/sandbox/REPLACE_THIS_VALUE"
+    headers = {"x-ibm-client-id": "REPLACE_THIS_VALUE"}
+    myResponse = requests.delete(url, headers = headers)
+    data= myResponse.content
+    print (data)
+    return (data)
+
+@app.route('/myaccounts')
+def myaccounts():
+    url = "https://apis.nbg.gr/public/sandbox/obp.account.sandbox/v1.1/obp/my/accounts"
+    headers = { "content-type": "application/json",
+                "accept": "application/json",
+                "x-ibm-client-id": "REPLACE_THIS_VALUE",
+                "sandbox_id": "REPLACE_THIS_VALUE",
+                "application_id": "REPLACE_THIS_VALUE",
+                "provider_username": "REPLACE_THIS_VALUE",
+                "provider_id": "REPLACE_THIS_VALUE",
+                "provider": "REPLACE_THIS_VALUE",
+                "request_id": "REPLACE_THIS_VALUE"
+                }
+    myResponse = requests.get(url, headers = headers)
+    data= myResponse.content
+    print (data)
+    return (data)
+
+if __name__== "__main__":
+	app.run(debug=True, host="0.0.0.0", port=5000)
